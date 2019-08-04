09fa68a031f827b837ad8dc2cbf8d25489857cb8
Wifsimster <battistella@gmail.com>
2019-08-04 10:20:59 +0200

    Remove axios from dependencies and add async/await syntax

--- yquem.js
+++ yquem.js
@@ -5,17 +5,20 @@
 
 const fs = require("fs")
 const path = require("path")
-const axios = require("axios")
 const isWithinInterval = require("date-fns/isWithinInterval")
 const subDays = require("date-fns/subDays")
 
-const API_KEY = "0b07bc22f051"
+const http = require("http")
+const https = require("https")
 
-const instance = axios.create({
-  baseURL: `http://api.betaseries.com/`,
-  timeout: 1000,
-  headers: { "X-BetaSeries-Version": "3.0", "X-BetaSeries-Key": API_KEY }
-})
+const API_KEY = "0b07bc22f051"
+const BASE_URL = `http://api.betaseries.com/`
+
+// const instance = axios.create({
+//   baseURL: `http://api.betaseries.com/`,
+//   timeout: 1000,
+//   headers: { "X-BetaSeries-Version": "3.0", "X-BetaSeries-Key": API_KEY }
+// })
 
 module.exports = class {
   constructor(dir, fileAge = 2) {
@@ -25,17 +28,17 @@ module.exports = class {
 
   run() {
     return new Promise((resolve, reject) => {
-      let files = this.getRecentFilesFromDirectory(this.dir, this.fileAge)
+      const files = this.getRecentFilesFromDirectory(this.dir, this.fileAge)
 
       if (files && files.length > 0) {
-        let promises = files.map(file => {
+        const promises = files.map(async file => {
           const tmp = file.split(`\\`)
           const episodePath = path.resolve(this.dir, tmp[0], tmp[1])
           const filename = tmp[tmp.length - 1]
           const name = this.getShowName(filename)
           const number = this.getShowNumber(filename)
 
-          return this.downloadSubtitle(episodePath, name, number)
+          return await this.downloadSubtitle(episodePath, name, number)
         })
 
         Promise.all(promises)
@@ -51,65 +54,91 @@ module.exports = class {
     })
   }
 
-  downloadSubtitle(episodePath, name, number) {
+  async download(url) {
     return new Promise((resolve, reject) => {
-      this.getShow(name)
-        .then(result => {
-          if (result.data && result.data.shows && result.data.shows.length > 0) {
-            let show = result.data.shows[0]
+      https
+        .get(url, response => {
+          const { statusCode } = response
 
-            this.getEpisodeByShow(show.id, `S${number.season}E${number.episode}`)
-              .then(result => {
-                if (result.data && result.data.episode) {
-                  let episode = result.data.episode
-                  if (episode.subtitles && episode.subtitles.length > 0) {
-                    let subtitle = episode.subtitles[1]
-                    if (subtitle && subtitle.url) {
-                      axios
-                        .get(subtitle.url)
-                        .then(result => {
-                          if (result.data) {
-                            let language = subtitle.language.toLowerCase()
-                            language = language === `vf` ? `fr` : `en`
-
-                            const filePath = path.resolve(
-                              `${episodePath}`,
-                              `${show.title} - ${number.season}x${number.episode}.${language}.srt`
-                            )
-
-                            this.writeFile(result.data, filePath)
-                              .then(filePath => {
-                                resolve(`${filePath} write with success !`)
-                              })
-                              .catch(err => {
-                                reject(`[Write subtitle] Error writeFile : ${err}`)
-                              })
-                          }
-                        })
-                        .catch(err => {
-                          reject(`[Download subtitle] Error : ${err}`)
-                        })
-                    } else {
-                      resolve(`${show.title} - ${episode.season}x${episode.episode} : Subtitle not found !`)
-                    }
-                  } else {
-                    resolve(`${show.title} - S${number.season}E${number.episode} : No subtitle found !`)
-                  }
-                } else {
-                  resolve(`Episode not found : "S${number.season}E${number.episode}" !`)
-                }
-              })
-              .catch(err => {
-                reject(`[Get episode] Error : ${err}`)
-              })
+          if (statusCode >= 400) {
+            reject(`[Download subtitle] Error : ${statusCode}`)
           } else {
-            resolve(`Show not found : "${name}" !`)
+            if (statusCode === 302) {
+              const { headers } = response
+              https
+                .get(headers.location, response => {
+                  let data = ""
+
+                  response.on("data", chunk => {
+                    data += chunk
+                  })
+
+                  response.on("end", () => {
+                    resolve(data)
+                  })
+                })
+                .on("error", err => {
+                  reject(`[Download subtitle] Error : ${err}`)
+                })
+            } else {
+              let data = ""
+
+              response.on("data", chunk => {
+                data += chunk
+              })
+
+              response.on("end", () => {
+                resolve(data)
+              })
+            }
           }
         })
-        .catch(err => {
-          reject(`[Get show] Error : ${err}`)
+        .on("error", err => {
+          reject(`[Download subtitle] Error : ${err}`)
         })
     })
+  }
+
+  async downloadSubtitle(episodePath, name, number) {
+    const resultsShow = await this.getShow(name)
+
+    if (resultsShow.shows && resultsShow.shows.length > 0) {
+      const show = resultsShow.shows[0]
+
+      const resultsEpisode = await this.getEpisodeByShow(show.id, `S${number.season}E${number.episode}`)
+
+      if (resultsEpisode.episode) {
+        const episode = resultsEpisode.episode
+
+        if (episode.subtitles && episode.subtitles.length > 0) {
+          const subtitle = resultsEpisode.episode.subtitles[1]
+
+          if (subtitle && subtitle.url) {
+            const fileData = await this.download(subtitle.url)
+
+            if (fileData) {
+              let language = subtitle.language.toLowerCase()
+              language = language === `vf` ? `fr` : `en`
+
+              const filePath = path.resolve(
+                `${episodePath}`,
+                `${show.title} - ${number.season}x${number.episode}.${language}.srt`
+              )
+
+              return await this.writeFile(fileData, filePath)
+            }
+          } else {
+            console.error(`${show.title} - ${episode.season}x${episode.episode} : Subtitle not found !`)
+          }
+        } else {
+          console.error(`${show.title} - S${number.season}E${number.episode} : No subtitle found !`)
+        }
+      } else {
+        console.error(`Episode not found : "S${number.season}E${number.episode}" !`)
+      }
+    } else {
+      console.error(`No show found for "${name}" !`)
+    }
   }
 
   getRecentFilesFromDirectory() {
@@ -156,11 +185,63 @@ module.exports = class {
   }
 
   getShow(title) {
-    return instance.get(`shows/search?title=${title}`)
+    return new Promise((resolve, reject) => {
+      http
+        .get(
+          `${BASE_URL}shows/search?title=${title}`,
+          { headers: { "X-BetaSeries-Version": "3.0", "X-BetaSeries-Key": API_KEY } },
+          response => {
+            const { statusCode } = response
+
+            if (statusCode < 400) {
+              let data = ""
+
+              response.on("data", chunk => {
+                data += chunk
+              })
+
+              response.on("end", () => {
+                resolve(JSON.parse(data))
+              })
+            } else {
+              reject(`[Download subtitle] Error : ${statusCode}`)
+            }
+          }
+        )
+        .on("error", err => {
+          reject(`[Download subtitle] Error : ${err}`)
+        })
+    })
   }
 
   getEpisodeByShow(id, number) {
-    return instance.get(`episodes/search?show_id=${id}&number=${number}&subtitles='vf'`)
+    return new Promise((resolve, reject) => {
+      http
+        .get(
+          `${BASE_URL}episodes/search?show_id=${id}&number=${number}&subtitles='vf'`,
+          { headers: { "X-BetaSeries-Version": "3.0", "X-BetaSeries-Key": API_KEY } },
+          response => {
+            const { statusCode } = response
+
+            if (statusCode < 400) {
+              let data = ""
+
+              response.on("data", chunk => {
+                data += chunk
+              })
+
+              response.on("end", () => {
+                resolve(JSON.parse(data))
+              })
+            } else {
+              reject(`[Download subtitle] Error : ${statusCode}`)
+            }
+          }
+        )
+        .on("error", err => {
+          reject(`[Download subtitle] Error : ${err}`)
+        })
+    })
   }
 
   writeFile(data, filenamePath) {
